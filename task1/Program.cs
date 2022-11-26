09db860cda79a489a7bdcc0e11a7080587582052
Realrett <gosha-student@mail.ru>
2022-11-26 10:25:41 +0700

    1 задача

---
+++ task1/Program.cs
@@ -0,0 +1,72 @@
+﻿/* Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле:
+ Aₘₙ = m+n. Выведите полученный массив на экран.
+m = 3, n = 4.
+0 1 2 3
+1 2 3 4
+2 3 4 5 */
+
+int[,] InitArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i,j] = i + j;
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; GetLength(0) < m; i++)
+    {
+        for (int j = 0; j < GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите число m: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число n: ");
+int n = int.Parse(Console.ReadLine());
+int[,] array = InitArray(m,n);
+PrintArray(array);
+
+
+
+int[,] InitArray(int m,int n)
+{
+    int[,] array = new int[m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i,j] = i + j;
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Введите число m: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число n: ");
+int n = int.Parse(Console.ReadLine());
+int[,] array = InitArray(m,n);
+PrintArray(array);
